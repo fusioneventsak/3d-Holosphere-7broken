ae50dc62d1c4a59a32397d118584cdb200358ede
fusioneventsak <arthurk@fusion-events.ca>
2025-06-10 11:52:32 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
 import CollageViewerPage from './pages/CollageViewerPage';
 import CollageEditorPage from './pages/CollageEditorPage';
 import LandingPage from './pages/LandingPage';
@@ -33,31 +32,6 @@ const App: React.FC = () => {
           {/* Redirect unknown routes to landing */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-        
-        {/* Toast notifications */}
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            duration: 4000,
-            style: {
-              background: '#1f2937',
-              color: '#fff',
-              border: '1px solid #374151',
-            },
-            success: {
-              iconTheme: {
-                primary: '#10b981',
-                secondary: '#fff',
-              },
-            },
-            error: {
-              iconTheme: {
-                primary: '#ef4444',
-                secondary: '#fff',
-              },
-            },
-          }}
-        />
       </div>
     </Router>
   );
